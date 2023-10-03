f8d06a8367418bb78bc6b252c0ab8a4d60deadef
JosePonferrada <joseponfe10@gmail.com>
2023-10-03 08:33:09 +0200

    fix(test)

--- src/iesMarquesDeComaresEntornosDeDesarrollo/HolaMundo.java
+++ src/iesMarquesDeComaresEntornosDeDesarrollo/HolaMundo.java
@@ -3,7 +3,8 @@ package iesMarquesDeComaresEntornosDeDesarrollo;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-System.out.println("Hola Mundo!");
+		System.out.println("Hola Mundo!");
+		System.out.println("Probando probando");
 	}
 
 }
