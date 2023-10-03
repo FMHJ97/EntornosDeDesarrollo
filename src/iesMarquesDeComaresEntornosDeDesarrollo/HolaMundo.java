c2e9de49fca12823dce14905f036978081b2286d
FRuiMar <franruiz81@gmail.com>
2023-10-03 09:02:20 +0200

    feat (añadiendo linea de prueba)

--- src/iesMarquesDeComaresEntornosDeDesarrollo/HolaMundo.java
+++ src/iesMarquesDeComaresEntornosDeDesarrollo/HolaMundo.java
@@ -5,6 +5,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!");
 		System.out.println("Probando probando");
+		System.out.println("Adios Mundo Cruel!");
+		
 	}
 
 }
